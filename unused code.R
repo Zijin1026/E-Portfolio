037132339552c8910594b3978d57bbc23aeacb4a
student <zijin.xiao@ubc.ca>
2026-03-02 22:09:33 -0800

    update

--- unused code.R
+++ unused code.R
@@ -1,123 +1,120 @@
-Data overview as follows:
+{r leaflet_4, include = FALSE}
+library(scales);library(terra)
+Sys.unsetenv("PROJ_LIB")
+Sys.unsetenv("GDAL_DATA")
+radius <- function(x){
+  scaled <- rescale(x, to = c(4,25))
+} 
+vec <- st_make_valid(vec)
+centroid <- st_centroid(vec)
+
+slope <- rast("images/geo_layers/Slope.tif")
+radpot <- rast("images/geo_layers/radpot.tif")
+twi <- rast("images/geo_layers/TWI.tif")
+
+
+
+dotmap <- leaflet(centroid)%>%
+  addProviderTiles("Esri.WorldImagery") %>%
+  addCircleMarkers(group = "CanopyDensityError",
+                   radius = ~radius(dff_CDn),
+                   fillColor =  ~pal2(dff_CDn),
+                   stroke = TRUE,
+                   color = "black", 
+                   weight = 0.25,
+                   fillOpacity = 1,
+                   popup = ~paste0("CanopyDensityError: ", dff_CDn))%>%
+  addCircleMarkers(group = "HeightError",
+                   radius = ~radius(diff_he),
+                   fillColor =  ~pal3(diff_he),
+                   stroke = TRUE,
+                   fillOpacity = 1,
+                   color = "black", 
+                   weight = 0.25,
+                   popup = ~paste0("CanopyDensityError: ", diff_he)) %>%
+  addCircleMarkers(group = "VolumeError",
+                   radius = ~radius(diff_vl),
+                   fillColor =  ~pal4(diff_vl),
+                   stroke = TRUE,
+                   fillOpacity = 1,
+                   color = "black", 
+                   weight = 0.25,
+                   popup = ~paste0("VolumeError: ", diff_vl)) %>%
+  addRasterImage(slope, 
+                 group = "slope") %>%
+  addRasterImage(radpot, 
+                 group = "radpot") %>%
+  addRasterImage(twi, 
+                 group = "twi") %>%
+  addLayersControl(overlayGroups = c("CanopyDensityError","HeightError","VolumeError"),
+                   baseGroups = c("slope","radpot","twi"),
+                   options = layersControlOptions(collapsed = TRUE))
+
+)
   
   
-```{r leaflet, include = FALSE}
-
-library(sf)
-library(dplyr)
-library(leaflet)
-library(htmltools)
-
-# 1) Read + prep shapefile
-shp <- st_read("Shiny/GVan_DA_Final.shp", quiet = TRUE) |>
-  st_transform(4326) |>
-  select(-any_of(c("DGUID","PRUID","Shape_Leng","Shape_Area"))) |>
-  rename(
-    "Land Area" = LANDAREA,
-    
-    "Distance to Stop" = StopDis,
-    "Distance to CBD" = CBDDis,
-    
-    "House Maintenance Fee" = H_maintain,
-    "House Rent" = H_rent,
-    
-    "Population Density" = popdensity,
-    "Green Density" = green_dens,
-    "High Education Count" = H_edu,
-    
-    "Age" = Age_,
-    "Unemployment Rate" = Unemploy_R,
-    "Median Income" = MedianInc,
-    
-    "Distance to Coast" = DIST_Coast,
-    "Distance to School" = dis_scho,
-    "Distance to Hospital/Clinic" = dis_medi,
-    
-    "Surface Temperature" = SurfaceT,
-    "PM2.5" = PM2_5_Mean,
-    "Crime Rate" = CrimeRate
-  )
-
-# 2) Popup builder (all columns, prettier)
-popup_allcols <- function(x){
-  att <- st_drop_geometry(x)
   
-  apply(att, 1, function(row){
-    tags$div(
-      style="font-size:13px; line-height:1.25;",
-      tags$b("Area Information"),
-      tags$hr(style="margin:6px 0;"),
-      tags$table(
-        style="border-collapse:collapse;",
-        lapply(names(row), function(nm){
-          val <- row[[nm]]
-          # format numbers nicely
-          if (suppressWarnings(!is.na(as.numeric(val)))) {
-            val <- format(round(as.numeric(val), 3), big.mark = ",", scientific = FALSE)
-          } else {
-            val <- as.character(val)
-          }
-          tags$tr(
-            tags$td(style="padding:2px 10px 2px 0; font-weight:600; vertical-align:top;", nm),
-            tags$td(style="padding:2px 0; vertical-align:top;", val)
-          )
-        })
-      )
-    ) |> as.character()
-  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  Code Snippets
+  
+  Sample code snippet. Notice that you can provide a toggle to switch between coding languages - this is referred to as a 'tabset' in quarto. It is good practice to try and convert your R code to python, and vice-versa to demonstrate coding proficiency. For example, let's showcase a function for calculating NDVI in R and Python.
+
+R
+
+calc_ndvi <- function(nir, red){
+  ndvi <- (nir-red)/(nir+red)
+  return(ndvi)
 }
 
-shp$popup <- popup_allcols(shp)
+(Assuming nir and red are terra rast objects)
 
-# 3) Map (auto geometry type)
-g <- unique(as.character(st_geometry_type(shp)))
+Python
 
-m <- leaflet(shp) |>
-  addTiles() |>
-  addScaleBar(position = "bottomleft")
+def calc_ndvi(nir, red): 
+  ndvi = (nir - red)/(nir + red)
+  return(ndvi)
 
-if (any(g %in% c("POLYGON","MULTIPOLYGON"))) {
-  m <- m |>
-    addPolygons(
-      weight = 0.6,              # thinner line
-      color  = "#1B3A6B",         # darker boundary
-      opacity = 0.9,
-      fillOpacity = 0.30,
-      smoothFactor = 0.5,
-      popup = ~popup,
-      highlightOptions = highlightOptions(
-        weight = 2,
-        color = "#FFD54F",
-        bringToFront = TRUE
-      )
-    )
-} else if (any(g %in% c("LINESTRING","MULTILINESTRING"))) {
-  m <- m |>
-    addPolylines(
-      weight = 1.2,
-      color = "#1B3A6B",
-      opacity = 0.9,
-      popup = ~popup,
-      highlightOptions = highlightOptions(
-        weight = 3,
-        color = "#FFD54F",
-        bringToFront = TRUE
-      )
-    )
-} else {
-  m <- m |>
-    addCircleMarkers(
-      radius = 4,
-      stroke = TRUE,
-      weight = 0.8,
-      color = "#1B3A6B",
-      opacity = 0.9,
-      fillOpacity = 0.6,
-      popup = ~popup
-    )
-}
-```
+(Assuming nir and red are numpy arrays)
 
-```{r map, echo=FALSE}
-m
-```
+External links
+
+You can also provide a frame linking to external websites. For example, here is a Google Earth Engine application I developed - which I embedded in this webpage using the code below:
+
+{code}
+
+<iframe width="900" height="700"
+src="https://ee-melserramon.projects.earthengine.app/view/thermal-analysis-tool">
+</iframe>
+
+The full-screen GEE application is available here in case you're interested.
+  
+  (To use the GEE tool, navigate to any city you'd like, hit apply filters, and click anywhere on the map to retrieve a time-series of Landsat surface temperature observations for that point. Areas where the maximum temp exceeded 35 degrees Celsius in your date-range are highlighted in red.)
+
+<iframe width="900" height="700" src="https://ee-melserramon.projects.earthengine.app/view/thermal-analysis-tool"></iframe>
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
